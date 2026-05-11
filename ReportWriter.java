6642ccdcd07244d0e49d33a963aedb14a2bb12d5
SudipStar <73497429+SudipKing@users.noreply.github.com>
2026-05-11 15:11:02 -0700

    need to decide on report formatting
    Purpose: Interface for report generators.

---
+++ ReportWriter.java
@@ -0,0 +1,9 @@
+package com.fsw.report;
+
+import com.fsw.model.FileEvent;
+import java.util.List;
+
+public interface ReportWriter {
+    void printToConsole(List<FileEvent> rows);
+    void writeToFile(List<FileEvent> rows, String name);
+}
