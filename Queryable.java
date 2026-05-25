8b5b863627b1339bb1129af81db6370b965f2ea3
SudipStar <73497429+SudipKing@users.noreply.github.com>
2026-05-24 20:22:20 -0700

    Queryable - cleaned and updated
    Changes:
    Renamed parameter f → filter
    More readable
    Better self documentation
    Helps maintainability in larger projects

---
+++ Queryable.java
@@ -0,0 +1,14 @@
+package com.fsw.database;
+
+import com.fsw.model.FileEvent;
+import com.fsw.query.QueryFilter;
+
+import java.util.List;
+
+public interface Queryable {
+
+    List<FileEvent> query(QueryFilter filter);
+
+    void exportToCsv(String path);
+
+}
